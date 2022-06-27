50248d50de18ed2e121c9f7abb44c852792d6b6c
Sergey Chebatoryov <fcc@qlik.com>
2022-06-27 15:44:59 +0300

    video without recycle viewer

--- app/src/main/java/com/example/disney_time02/Movie_Page.java
+++ app/src/main/java/com/example/disney_time02/Movie_Page.java
@@ -1,22 +1,25 @@
 package com.example.disney_time02;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Looper;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.Vector;
 
 public class Movie_Page extends AppCompatActivity {
-    RecyclerView recyclerView;
-    Vector<YouTubeVideos> youtubeVideos = new Vector<>();
+    Activity mainActivity = this; // If you are in activity
+    WebView videoWeb;
     private String movieId;
     private String movieName;
     boolean switchChecked;
@@ -51,27 +54,21 @@ public class Movie_Page extends AppCompatActivity {
         switchCompat.setChecked(savedMovie != null);
         switchCompat.setOnCheckedChangeListener((buttonView, isChecked) -> save(isChecked));
 
-        //Create and show spinner dialog
+        //Create spinner dialog
         dialog = new AlertDialog.Builder(this)
                 .setView(R.layout.layout_loading_dialog)
                 .create();
-        dialog.show();
-        //Insert move to movie box with spinner
-        recyclerView = findViewById(R.id.trailerView);
-        recyclerView.requestFocus();
-        recyclerView.setHasFixedSize(false);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        new Thread(this::prepareMovieView).start();
+
+        //prepare video player
+        getVideoView();
+        prepareMovieView();
     }
 
     private void prepareMovieView() {
         String movieUrl = getIntent().getExtras().getString("url", "");
         String url = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/" +
                 getId(movieUrl) + "\" frameborder=\"0\" allowfullscreen></iframe>";
-        youtubeVideos.add(new YouTubeVideos(url));
-        VideoAdapter videoAdapter = new VideoAdapter(youtubeVideos, this);
-        recyclerView.setAdapter(videoAdapter);
-        dialog.dismiss();
+        videoWeb.loadData( url, "text/html" , "utf-8");
     }
 
     public void save(boolean isChecked) {
@@ -103,4 +100,46 @@ public class Movie_Page extends AppCompatActivity {
     public void goBack(View view) {
         finish();
     }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void getVideoView(){
+        videoWeb = findViewById(R.id.trailerView);
+        videoWeb.getSettings().setJavaScriptEnabled(true);
+        videoWeb.setWebChromeClient(new WebChromeClient() {
+            private View mCustomView;
+            private WebChromeClient.CustomViewCallback mCustomViewCallback;
+            private int mOriginalOrientation;
+            private int mOriginalSystemUiVisibility;
+
+            public Bitmap getDefaultVideoPoster() {
+                if (mainActivity == null) {
+                    return null;
+                }
+                return BitmapFactory.decodeResource(mainActivity.getApplicationContext().getResources(), 2130837573);
+            }
+
+            public void onHideCustomView() {
+                ((FrameLayout) mainActivity.getWindow().getDecorView()).removeView(this.mCustomView);
+                this.mCustomView = null;
+                mainActivity.getWindow().getDecorView().setSystemUiVisibility(this.mOriginalSystemUiVisibility);
+                mainActivity.setRequestedOrientation(this.mOriginalOrientation);
+                this.mCustomViewCallback.onCustomViewHidden();
+                this.mCustomViewCallback = null;
+            }
+
+            public void onShowCustomView(View paramView, WebChromeClient.CustomViewCallback paramCustomViewCallback) {
+                if (this.mCustomView != null) {
+                    onHideCustomView();
+                    return;
+                }
+                this.mCustomView = paramView;
+                this.mOriginalSystemUiVisibility = mainActivity.getWindow().getDecorView().getSystemUiVisibility();
+                this.mOriginalOrientation = mainActivity.getRequestedOrientation();
+                this.mCustomViewCallback = paramCustomViewCallback;
+                ((FrameLayout) mainActivity.getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
+                mainActivity.getWindow().getDecorView().setSystemUiVisibility(3846 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            }
+        });
+    }
+
 }
