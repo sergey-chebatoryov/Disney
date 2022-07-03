affdafd2ad70384a58ed159fff53d3785d005ea5
Sergey Chebatoryov <fcc@qlik.com>
2022-07-03 16:35:47 +0300

    call contacts for phone number

---
+++ app/src/main/java/com/example/disney_time02/DialogMultiplyNumbers.java
@@ -0,0 +1,63 @@
+package com.example.disney_time02;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Map;
+
+public class DialogMultiplyNumbers extends AppCompatActivity {
+    Map<String, String> numberType;
+    private final EditText edNumber;
+    private final Context context;
+
+    public DialogMultiplyNumbers(Map<String, String> numberType, EditText edNumber, Context context) {
+        this.numberType = numberType;
+        this.edNumber = edNumber;
+        this.context = context;
+    }
+    @SuppressLint("SetTextI18n")
+    public void makeDialogForMultipleNumbers(String name) {
+        if (numberType.size() == 1) {
+            edNumber.setText(numberType.entrySet().iterator().next().getValue());
+            return;
+        }
+        final Dialog dialog = new Dialog(context);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setContentView(R.layout.dialog_multiple_numbers);
+        RadioGroup radioGroup = dialog.findViewById(R.id.multipleNumberGroup);
+        TextView displayName = dialog.findViewById(R.id.displayName);
+        displayName.setText(name);
+        WindowManager.LayoutParams lWindowParams = new WindowManager.LayoutParams();
+        lWindowParams.copyFrom(dialog.getWindow().getAttributes());
+        lWindowParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lWindowParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        dialog.getWindow().setAttributes(lWindowParams);
+
+        numberType.keySet().forEach(key -> {
+            RadioButton radioButton = new RadioButton(context);
+            radioButton.setText(numberType.get(key) + "\n" + key);
+            radioButton.setPadding(0, 15, 0, 15);
+            radioGroup.addView(radioButton);
+        });
+        dialog.show();
+
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            RadioButton checkedRadio = group.findViewById(group.getCheckedRadioButtonId());
+            String num = checkedRadio.getText().toString();
+            edNumber.setText(num.substring(0, num.indexOf('\n')));
+            dialog.dismiss();
+        });
+    }
+}
--- app/src/main/java/com/example/disney_time02/Invite_Friend.java
+++ app/src/main/java/com/example/disney_time02/Invite_Friend.java
@@ -1,9 +1,15 @@
 package com.example.disney_time02;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,23 +19,32 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-public class Invite_Friend extends AppCompatActivity/* implements AdapterView.OnItemSelectedListener */{
+public class Invite_Friend extends AppCompatActivity {
+    public static final int REQUEST_READ_CONTACTS = 79;
     private EditText edNumber;
     private Spinner spinner;
     AlertDialog dialog;
-    List<String> rowStrings;
-    ArrayAdapter<String> adapter;
+    List<String> movies;
+    ArrayAdapter<String> adapterMovie;
     Map<Integer, Map<String, String>> moviesMap;
+    Map<String, String> numberType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +52,15 @@ public class Invite_Friend extends AppCompatActivity/* implements AdapterView.On
         setContentView(R.layout.activity_invite_friend);
         setActionBar("");
         Button btnMessage = findViewById(R.id.message);
-        this.edNumber = findViewById(R.id.number);
 
+        //Create dropdown box for movie
         spinner = findViewById(R.id.movie);
-        rowStrings = new ArrayList<>();
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, rowStrings);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        movies = new ArrayList<>();
+        adapterMovie = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, movies);
+        adapterMovie.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapterMovie);
+
+        edNumber = findViewById(R.id.number);
 
         dialog = new AlertDialog.Builder(this)
                 .setView(R.layout.layout_loading_dialog)
@@ -68,9 +85,9 @@ public class Invite_Friend extends AppCompatActivity/* implements AdapterView.On
         moviesMap = mysqlConnect.select("SELECT b.id, a.name, a.genre, trailer FROM movies a JOIN usersmovie b ON b.id=a.id where b.name='" + LoginActivity.userName + "'", this);
         Collection<Map<String, String>> rows = moviesMap.values();
         for (Map<String, String> row : rows) {
-            rowStrings.add(row.get("name"));
+            movies.add(row.get("name"));
         }
-        this.runOnUiThread(() -> adapter.notifyDataSetChanged());
+        this.runOnUiThread(() -> adapterMovie.notifyDataSetChanged());
 
         dialog.dismiss();
     }
@@ -101,6 +118,88 @@ public class Invite_Friend extends AppCompatActivity/* implements AdapterView.On
         MenuClass menuClass = new MenuClass(this);
         return menuClass.getItemSelected(item);
     }
+
+    //On click to edit box 'number' call telephone book
+    public void goContacts(View view) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+            mStartForResult.launch(contactPickerIntent);
+        } else {
+            requestPermission();
+        }
+
+    }
+
+    //Request permission to read contacts
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_CONTACTS)) {
+            runPermissionDialog();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    android.Manifest.permission.READ_CONTACTS}, REQUEST_READ_CONTACTS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_READ_CONTACTS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                mStartForResult.launch(contactPickerIntent);
+            }
+        }
+    }
+
+    //If permission denied ask to set permission manually
+    private void runPermissionDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Grant Permission");
+        builder.setMessage(" Read contacts permission denied.\nDo you want to grant it?");
+        builder.setPositiveButton("yes", (dialog, which) -> {
+                    startActivity(new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.parse("package:" + BuildConfig.APPLICATION_ID)));
+                    dialog.dismiss();
+                })
+                .setNegativeButton("no", (dialog, which) -> dialog.dismiss());
+        builder.create().show();
+    }
+
+    //Read phone book call result
+    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @SuppressLint("Range")
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent intent = result.getData();
+                        Uri contactData = intent != null ? intent.getData() : null;
+                        ContentResolver cr = getContentResolver();
+                        Cursor cur = cr.query(contactData, null, null, null, null);
+                        if (cur.getCount() > 0) {
+                            if (cur.moveToNext()) {
+                                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                                if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                                    // Query phone here.
+                                    Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id, null, null);
+                                    numberType = new HashMap<>();
+                                    while (phones.moveToNext()) {
+                                        String phoneNum = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                                        String type = ContactsContract.CommonDataKinds.Phone.getTypeLabel(getResources(), phones.getInt(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE)), "").toString();
+                                        numberType.put(type, phoneNum);
+                                    }
+                                    //Show dialog to choose number for multi-number contact
+                                    DialogMultiplyNumbers dialogMultiplyNumbers = new DialogMultiplyNumbers(numberType, edNumber, Invite_Friend.this);
+                                    dialogMultiplyNumbers.makeDialogForMultipleNumbers(name);
+                                    phones.close();
+                                }
+                            }
+                        }
+                        cur.close();
+                    }
+                }
+            });
 
     public void goBack(View view) {
         finish();
